56995859e26da51cc8475a3e294a2d0996ea4ac5
ItzelMaldonado <itzel.mn@gmail.com>
2024-07-08 15:56:53 +0200

    cors api docu

--- src/main/java/be/vdab/cors/CorsApplication.java
+++ src/main/java/be/vdab/cors/CorsApplication.java
@@ -1,10 +1,18 @@
 package be.vdab.cors;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springdoc.webmvc.api.OpenApiActuatorResource;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class CorsApplication {
+    @Bean
+    OpenAPI openApi(){
+        return new OpenAPI().info(new Info().title("Rekenen").version("1.0.0").description("Berekeningen maken"));
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(CorsApplication.class, args);
