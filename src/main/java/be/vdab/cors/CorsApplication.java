23f874798264ffcf30fb4d64d3f2c8dc45474790
ItzelMaldonado <itzel.mn@gmail.com>
2024-07-09 09:33:02 +0200

    dependencies en annotations voor openapi documentatie

--- src/main/java/be/vdab/cors/CorsApplication.java
+++ src/main/java/be/vdab/cors/CorsApplication.java
@@ -1,10 +1,17 @@
 package be.vdab.cors;
 
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class CorsApplication {
+    @Bean
+    OpenAPI openAPI(){
+        return new OpenAPI().info(new Info().title("Rekenen").version("1.0.0").description("Berekeningen maken"));
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(CorsApplication.class, args);
